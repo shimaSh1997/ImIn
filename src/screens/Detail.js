bda5d9f52fdd614790c764f345241b34ef70db84
shima <shima.wh.2489@gmail.com>
2018-09-15 16:37:44 +0430

    create Detail Activity setup

---
+++ src/screens/Detail.js
@@ -0,0 +1,32 @@
+import React ,{ Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+
+class Detail extends Component{
+    static navigationOptions = {
+        header: null
+    }
+    render(){
+        return(
+            <View style={styles.container}>
+               
+            </View>
+
+        )
+    }
+}
+
+export default Detail;
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        
+    }
+})

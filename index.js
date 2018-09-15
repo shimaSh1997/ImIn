695c4632c7e3a5a3af340e66064710cc7cf93674
shima <shima.wh.2489@gmail.com>
2018-09-15 16:07:19 +0430

    setup Redux

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
+import { Provider } from 'react-redux';
+
+const redux = () => (
+    <Provider>
+        <App />
+    </Provider>
+)
 
 
-AppRegistry.registerComponent("ImIn", () => App);
+AppRegistry.registerComponent("ImIn", () => redux);
